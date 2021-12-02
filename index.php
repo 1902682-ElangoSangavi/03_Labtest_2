2d7aa1cec6e297034523c8b0ad6cf256b46726e9
sangasangasanga <sangavi1999@live.com>
2021-12-02 18:45:52 +0800

    add initial files

---
+++ index.php
@@ -0,0 +1,7 @@
+<form action="validate.php" method="POST">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Search</label>
+    <input type="text" class="form-control" name="input1" id="input1">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
